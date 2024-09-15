f126411fb977273f5bebc754bfe04968023a259b
Prajakta Yadav <prajaktayadav199@gmail.com>
2024-09-15 23:10:39 +0530

    Learn More details about classes and this

--- data/products.js
+++ data/products.js
@@ -54,6 +54,35 @@ class Clothing extends Product {
   }
 }
 
+/*const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
+/*console.log(this);
+
+const obj2 = {
+  a: 2,
+  b: this.a
+}*/
+/*
+function logThis()
+{
+  console.log(this);
+}
+
+logThis();
+logThis.call('Hello');
+
+
+const obj3 = {
+  method: () => {
+    console.log(this);
+  }
+
+};
+obj3.method();
+*/
 
 export const products = [
   {
